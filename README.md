5e23eada6fa970433a7137094d9917e30cc496ab
jharris711 <49429928+jharris711@users.noreply.github.com>
2023-06-06 09:16:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tw-noti-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/tw-noti-demo)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import Content from './Content';
+import { ToastProvider } from 'tw-noti';
+
+export default function App() {
+  return (
+    <>
+      <ToastProvider>
+        <Content />
+      </ToastProvider>
+    </>
+  );
+}
