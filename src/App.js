eb838b28c47b06c7b6eb5515f3565fa267d8dfb3
jharris711 <49429928+jharris711@users.noreply.github.com>
2023-06-07 01:37:56 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { ToastProvider } from 'tw-noti';
 export default function App() {
   return (
     <>
-      <ToastProvider containerClasses="top-12" messageClasses="font-extrabold">
+      <ToastProvider
+        containerClasses="top-12 right-12"
+        messageClasses="font-extrabold"
+      >
         <Content />
       </ToastProvider>
     </>
